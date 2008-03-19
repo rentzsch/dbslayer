c9a9bf67d5b8e769c75c78b54c265187c52a22a7
dgottfrid <dgottfrid@2fb5fb43-31c4-4d57-82c2-18ac32159141>
2008-03-19 23:02:47 +0000

    switching from apr_hash to json_skip_list

--- include/slayer_server_logging.h
+++ include/slayer_server_logging.h
@@ -1,5 +1,5 @@
-#ifndef _DBSLAYER_LOGGING_H_
-#define _DBSLAYER_LOGGING_H_
+#ifndef _SLAYER_LOGGING_H_
+#define _SLAYER_LOGGING_H_
 
 #include <apr_thread_proc.h>
 #include <apr_queue.h>
@@ -10,40 +10,40 @@
 #include <apr_strings.h>
 #include "simplejson.h"
 
-/* $Id: dbslayer_logging.h,v 1.4 2007/05/09 20:55:00 derek Exp $ */
+/* $Id: slayer_server_logging.h,v 1.1 2008/02/29 00:18:53 derek Exp $ */
 
-typedef struct _query_entry_t { 
+typedef struct _query_entry_t {
 	char *json_view;
 } query_entry_t;
 
-typedef struct _dbslayer_log_manager_t { 
+typedef struct _slayer_server_log_manager_t {
 	apr_pool_t *mpool;
-	apr_thread_mutex_t *file_mutex; 
+	apr_thread_mutex_t *file_mutex;
 	apr_file_t *fhandle;
 	query_entry_t *entries;
 	int offset;
 	int nentries;
-	apr_thread_mutex_t *list_mutex; 
-} dbslayer_log_manager_t;
+	apr_thread_mutex_t *list_mutex;
+} slayer_server_log_manager_t;
 
-int dbslayer_log_open(dbslayer_log_manager_t **manager,const char *filename, int nentries, apr_pool_t *mpool);
-int dbslayer_log_message(dbslayer_log_manager_t *manager, const char *message);
-int dbslayer_log_close(dbslayer_log_manager_t *manager);
+int slayer_server_log_open(slayer_server_log_manager_t **manager,const char *filename, int nentries, apr_pool_t *mpool);
+int slayer_server_log_message(slayer_server_log_manager_t *manager, const char *message);
+int slayer_server_log_close(slayer_server_log_manager_t *manager);
 
-int dbslayer_log_request(dbslayer_log_manager_t *manager, apr_pool_t *mpool, apr_socket_t *conn, 
-		const char *request_line, int response_code, int nbytes_sent, apr_int64_t time_toservices);
+int slayer_server_log_request(slayer_server_log_manager_t *manager, apr_pool_t *mpool, apr_socket_t *conn,
+                               const char *request_line, int response_code, int nbytes_sent, apr_int64_t time_toservices);
 
-int dbslayer_log_err_message(dbslayer_log_manager_t *manager,apr_pool_t *mpool,apr_socket_t *conn, 
-		const char *request_line, const char * message);
+int slayer_server_log_err_message(slayer_server_log_manager_t *manager,apr_pool_t *mpool,apr_socket_t *conn,
+                                   const char *request_line, const char * message);
 ///
-void dbslayer_log_add_entry(dbslayer_log_manager_t *manager, apr_pool_t *mpool,
-			const char *client_ip,apr_int64_t rtime,
-			const char *request_line,int response_code, 
-			int nbytes_sent, apr_int64_t time_toservice );
+void slayer_server_log_add_entry(slayer_server_log_manager_t *manager, apr_pool_t *mpool,
+                                  const char *client_ip,apr_int64_t rtime,
+                                  const char *request_line,int response_code,
+                                  int nbytes_sent, apr_int64_t time_toservice );
 
-void dbslayer_log_add_error(dbslayer_log_manager_t *manager, apr_pool_t *mpool,
-			const char *client_ip,apr_int64_t rtime,
-			const char *request_line, const char *error_msg );
+void slayer_server_log_add_error(slayer_server_log_manager_t *manager, apr_pool_t *mpool,
+                                  const char *client_ip,apr_int64_t rtime,
+                                  const char *request_line, const char *error_msg );
 
-char * dbslayer_log_get_entries(dbslayer_log_manager_t *manager, apr_pool_t *mpool);
-#endif /*_DBSLAYER_LOGGING_H_*/
+char * slayer_server_log_get_entries(slayer_server_log_manager_t *manager, apr_pool_t *mpool);
+#endif /*_SLAYER_LOGGING_H_*/
--- include/slayer_server_stats.h
+++ include/slayer_server_stats.h
@@ -1,4 +1,5 @@
-#ifndef _DBSLAYER_STATS_H_
+#ifndef _SLAYER_SERVER_STATS_H_
+#define _SLAYER_SERVER_STATS_H_
 #include <apr_thread_proc.h>
 #include <apr_queue.h>
 #include <apr_poll.h>
@@ -7,25 +8,25 @@
 #include <apr_file_info.h>
 #include <apr_strings.h>
 
-/* $Id: dbslayer_stats.h,v 1.3 2007/05/09 20:55:01 derek Exp $ */
+/* $Id: slayer_server_stats.h,v 1.1 2008/02/29 00:18:53 derek Exp $ */
 
-typedef struct _dbslayer_stats_t {
-  int *hits;
-  time_t *slices;
+typedef struct _slayer_server_stats_t {
+	int *hits;
+	time_t *slices;
 	time_t start_time;
 	int total_requests;
 	int offset;
-	apr_thread_mutex_t *mutex; 
+	apr_thread_mutex_t *mutex;
 	apr_pool_t *mpool;
 	int nslice;
 	int tslice;
-} dbslayer_stats_t;
+} slayer_server_stats_t;
 
-dbslayer_stats_t * dbslayer_stat_init(apr_pool_t *mpool,int nsclice, int tslice);
-dbslayer_stats_t * dbslayer_stat_init(apr_pool_t *mpool,int nsclice, int tslice) ;
-void dbslayer_stats_get(dbslayer_stats_t *istats, dbslayer_stats_t *out);
-void dbslayer_stats_tick(dbslayer_stats_t *stats);
-void dbslayer_stat_update(dbslayer_stats_t *stats);
-void dbslayer_stats_timer_thread(apr_pool_t *mpool,dbslayer_stats_t *stats);
-char * dbslayer_stats_tojson(dbslayer_stats_t *istats,apr_pool_t *mpool);
-#endif //_DBSLAYER_STATS_H_
+slayer_server_stats_t * slayer_server_stat_init(apr_pool_t *mpool,int nsclice, int tslice);
+slayer_server_stats_t * slayer_server_stat_init(apr_pool_t *mpool,int nsclice, int tslice) ;
+void slayer_server_stats_get(slayer_server_stats_t *istats, slayer_server_stats_t *out);
+void slayer_server_stats_tick(slayer_server_stats_t *stats);
+void slayer_server_stat_update(slayer_server_stats_t *stats);
+void slayer_server_stats_timer_thread(apr_pool_t *mpool,slayer_server_stats_t *stats);
+char * slayer_server_stats_tojson(slayer_server_stats_t *istats,apr_pool_t *mpool);
+#endif //_SLAYER_SERVER_STATS_H_
